1f35125867f99e23b6a68e163c792d9e0ef89481
Easonxiao <630478562@qq.com>
2020-09-08 22:39:48 +0800

    test122

--- mydevnet/demo1/打开文件.py
+++ mydevnet/demo1/打开文件.py
@@ -5,5 +5,30 @@
 
 #encoding ： 打开文件的编码方式,在Windows 里面， 默认用gbk打开
 
-file = open('xx.txt', encoding='utf-8')
+import os
+
+#1. 绝对的路径.
+
+import OS
+# print（os.name） # NT/posix
+# windows系统里，文件夹之间使用\分隔.
+# .在非windows系统里，文件夹之间使用/分隔.
+# os.sep . I
+#在Python的字符串里 ，\表示转义字符
+# file = open（ 'C:\\Users \ \chris\ \Desktop\ \Python基础\ \Day13-文件操作\01 -代码\ \xxx. txt'）
+# file = open（r'C: \Users \chris \Desktop \Python基础\Day13-文件操作01 -代码\xxx. txt'）
+#file = open('C:/Users/ chris/Desktop/ Python基础/Day13-文件操作/01-代码/xxx. txt')   ----- 推荐
+
+
+# 2.相对路径: 当前文件所在文件夹开始
+#../ 表示上级文件夹
+#./ 表示当前文件夹
+
+
+#file = open('xxx.txt')
+
+
+file = open('xx.txt', encoding='utf8')
 file.close()
+
+
---
+++ mydevnet/demo1/矩形.py
@@ -0,0 +1,32 @@
+#设计两个类:
+# -一个点类，属性包括x ， y坐标。
+#一-个Rectangle类（矩形） ，属性有左上角和右下角的坐标，
+#方法:1.计算矩形的面积；2. 判断点是否在矩形内
+#实例化一个点对象，一个正方形对象，输出矩形的面积，输出点是否在矩形内
+
+import random
+
+class Point(object):
+    def __init__(self, x:int,y:int):
+        self.x = x
+        self.y = y
+
+
+class Rectangle(object):
+    def __init__(self, top:tuple,bottom:tuple):
+        self.top = top
+        self.bottom = bottom
+
+    def area(self):
+        long = self.bottom[0]-self.top[0]
+        weith = self.top[1] - self.bottom[1]
+        return long * weith
+
+P1 = Point(random.randint(1,10),random.randint(1,10))
+R1= Rectangle((3,7), (4, 5))
+
+print((P1.x,P1.y))
+if R1.top[0]<= P1.x <= R1.bottom[0] and R1.bottom[1]<= P1.y <= R1.top[1]:
+    print(f"输入的矩形面积为：{R1.area()}, 点位置在矩形内部")
+else:
+    print(f"输入的矩形面积为：{R1.area()}, 点位置不在矩形内部")
